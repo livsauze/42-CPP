9d8225611d7c43f8d5e7886d4d65653129864bf2
livsauze <sauzetmartinlivane@hotmail.com>
2024-10-12 17:39:29 +0200

    adding ex02

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: livsauze <livsauze@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/12 17:14:02 by livsauze          #+#    #+#             */
+/*   Updated: 2024/10/12 17:39:08 by livsauze         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string* ptr = &str;
+	std::string& ref = str;
+
+	std::cout << "Memory adress of the string variable : " << &str << std::endl;
+	std::cout << "Memory adress held by ptr : " << ptr << std::endl;
+	std::cout << "Memory adress held by ref : " << &ref << std::endl;
+	std::cout << std::endl;
+	std::cout << "Value of the variable : " << str << std::endl;
+	std::cout << "Value pointed by ptr : " << *ptr << std::endl;
+	std::cout << "Value pointed by ref : " << ref << std::endl;
+
+}
