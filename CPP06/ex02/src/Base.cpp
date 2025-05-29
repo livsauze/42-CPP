f09f59f91a02449ab624225992074cec361462b6
Livane Sauzet <livsauze@c3r1p4.42perpignan.fr>
2025-05-29 17:41:52 +0200

    dynamic cast ok, ex02 finished, no leaks

--- CPP06/ex02/src/Base.cpp
+++ CPP06/ex02/src/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: livsauze <livsauze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 17:41:50 by livsauze          #+#    #+#             */
-/*   Updated: 2025/05/28 18:01:55 by livsauze         ###   ########.fr       */
+/*   Updated: 2025/05/29 17:36:02 by livsauze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 Base::~Base()
 {
 }
-
+// Generating a random object of type A/B/C
 Base* generate(void)
 {
 	int random = rand() % 3;
@@ -36,4 +36,53 @@ Base* generate(void)
 			return (new C);
 	}
 	return NULL;
+}
+// Identying the type of the object. dynamic_cast<> is used for derived classes
+// With pointers we can check if NULL is returned
+void identify(Base* p)
+{
+	if (dynamic_cast<A*>(p) != NULL)
+	{
+		std::cout << "Object of type A identified" << std::endl;
+		return;
+	}
+	else if (dynamic_cast<B*>(p) != NULL)
+	{
+		std::cout << "Object of type B identified" << std::endl;
+		return;
+	}
+	else if (dynamic_cast<C*>(p) != NULL)
+	{
+		std::cout << "Object of type C identified" << std::endl;
+		return;
+	}
+	std::cout << "Error : Unknown type" << std::endl;
+}
+
+// With references we have to catch exceptions
+// <typeinfo> is forbidden, we can't catch the bad_cast exception so we catch any exception
+void identify(Base& p)
+{
+	try
+	{
+		(void)dynamic_cast<A&>(p);
+		std::cout << "Object of type A identified" << std::endl;
+		return;
+	}
+	catch (...){}
+	try
+	{
+		(void)dynamic_cast<B&>(p);
+		std::cout << "Object of type B identified" << std::endl;
+		return;
+	}
+	catch (...){}
+	try
+	{
+		(void)dynamic_cast<C&>(p);
+		std::cout << "Object of type C identified" << std::endl;
+		return;
+	}
+	catch (...){}
+	std::cout << "Error : Unknown type" << std::endl;
 }
--- CPP06/ex02/src/main.cpp
+++ CPP06/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: livsauze <livsauze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 17:30:54 by livsauze          #+#    #+#             */
-/*   Updated: 2025/05/28 18:02:51 by livsauze         ###   ########.fr       */
+/*   Updated: 2025/05/29 17:40:54 by livsauze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 int main(void)
 {
 	std::srand(std::time(0));
+	std::cout << "It's time to generate an object !" << std::endl;
 	Base *randomBase = generate();
+	std::cout << "\nIdentiying with pointers :" << std::endl;
+	identify(randomBase);
+	std::cout << "\nIdentiying with references :" << std::endl;
+	identify(*randomBase);
 	delete randomBase;
 }
